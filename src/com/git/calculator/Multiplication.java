d5a3a42b99f2bb85ebe44f599aa404d31bf9b929
Sumit Ghosh <AB36194@host.docker.internal>
2022-01-07 17:00:01 +0530

    Updated values

--- src/com/git/calculator/Multiplication.java
+++ src/com/git/calculator/Multiplication.java
@@ -3,8 +3,8 @@ package com.git.calculator;
 public class Multiplication {
 		
 	public static void main(String arg) {
-		int a=5;
-		int b=10;
+		int a=15;
+		int b=100;
 		
 		System.out.println("Multiplication is: "+(a*b));
 	}

bec5671eb965e4028239a1cd172eabbccdb79b95
Sumit Ghosh <AB36194@host.docker.internal>
2022-01-07 17:17:41 +0530

    Finish test case creation for US USBPM78

--- src/com/github/stepDefination.java
+++ src/com/github/stepDefination.java
@@ -1,5 +1,10 @@
 package com.github;
 
 public class stepDefination {
-
+	public static void main(String arg) {
+		float a=5.5F;
+		float b=10.5F;
+		
+		System.out.println("Multiplication is: "+(a*b));
+	}
 }

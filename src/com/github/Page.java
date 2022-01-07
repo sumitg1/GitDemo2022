37852bd58f79d025a2cd10d4ccc5af133166e30b
Sumit Ghosh <AB36194@host.docker.internal>
2022-01-07 16:40:06 +0530

    Adding Float

--- src/com/github/Page.java
+++ src/com/github/Page.java
@@ -1,5 +1,12 @@
 package com.github;
 
 public class Page {
+	
+	public static void main(String arg) {
+		float a=5.5F;
+		float b=10.5F;
+		
+		System.out.println("Multiplication is: "+(a*b));
+	}
 
 }

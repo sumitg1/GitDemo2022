e53dad11cd2c390c2f085e2e1ffc4d353c7834a4
Sumit Ghosh <sumit_4ever@rediffmail.com>
2022-01-07 17:33:40 +0530

    Update Multiplication.java

--- src/com/git/calculator/Multiplication.java
+++ src/com/git/calculator/Multiplication.java
@@ -1,13 +1,24 @@
 package com.git.calculator;
 
 public class Multiplication {
-		
-	public static void main(String arg) {
-		int a=15;
-		int b=100;
-		
-		System.out.println("Multiplication is: "+(a*b));
-	}
 	
+public static void main(String arg[]) {		
+		System.setProperty("webdriver.chrome.driver", "C:\\Training-PPT\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		/*
+		driver.findElement(By.id("")).getAttribute("");
+		driver.findElement(By.id("")).getText();
+		driver.findElement(By.id("")).getTagName();
+		driver.findElement(By.id("")).submit();
+		*/
+		driver.get("https://demoqa.com/text-box");
+		List<WebElement> e=driver.findElements(By.tagName("input"));
+		System.out.println(e.size());
+		e.get(0).sendKeys("Hello");
+		e.get(1).sendKeys("My Name is Sumit");		
+		System.out.println(e.get(0).getAttribute("placeholder"));
+		System.out.println(e.get(0).getTagName());
+				
+	}
 
 }
